172fc64b95995b54dde7f72b770848a60b4cbce2
nicopierro <pierro.nicolo@iisgalvanimi.edu.it>
2021-09-23 09:23:27 +0000

    fix 01

---
+++ ReLab-new/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'server mappe';
+  //Aggiungiamo latitudine e longitudine di un luogo
+  center : any;
+  position : any;
+  label :string;
+  constructor()
+  {
+    this.center={lat: 45.506738, lng: 9.190766};
+    this.position = this.center;
+    this.label = "ciao";
+  }
+
+}
