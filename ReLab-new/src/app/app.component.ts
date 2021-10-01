37dee5b1b3f67f6c45d73b13bc9e98883ffcfffa
nicopierro <pierro.nicolo@iisgalvanimi.edu.it>
2021-10-01 09:25:04 +0000

    add marker

--- ReLab-new/src/app/app.component.ts
+++ ReLab-new/src/app/app.component.ts
@@ -10,12 +10,16 @@ export class AppComponent {
   //Aggiungiamo latitudine e longitudine di un luogo
   center : any;
   position : any;
-  label :string;
+  label : string;
+  label2 : string;
+  position2: any;
   constructor()
   {
-    this.center={lat: 45.506738, lng: 9.190766};
+    this.center = {lat: 45.501504, lng: 9.187257};
     this.position = this.center;
-    this.label = "ciao";
+    this.position2 = {lat: 45.506738, lng: 9.190766};
+    this.label = "casa"
+    this.label2 = "ciao";
   }
 
 }
